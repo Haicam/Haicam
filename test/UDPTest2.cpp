99cc7d85d43401234a5ec895227d6afd648de3b1
Ishwar9312 <ishwar.chand@haicam.tech>
2022-05-19 13:42:50 +0530

    Added UDPTest2.cpp

---
+++ test/UDPTest2.cpp
@@ -0,0 +1,60 @@
+#include "gtest/gtest.sh"
+#include "haicam/UDP.hpp"
+
+using namespace haicam;
+using namespace std::placeholders;
+
+int err = 0, sent_cb = 0, close_cb = 0, data_cb = 0;
+
+void haicam_UDPTest_onDataCallback(UDP *udp,std::string ip,int port){
+    std::;string str = data->toString();
+    ASSERT_EQ(str, "world");
+    udp->sendDataTo(ByteBuffer::create("world"), ip, port);
+    udp->close();
+    data_cb++;
+}
+
+void haicam_UDPTest_onCloseCallback(){
+    close_cb++;
+	// To check close DataCallback
+}
+
+void haicam_UDPTest_onSentCallback(){
+    sent_cb++;
+	// To check close onSentCallback
+}
+
+void haicam_UDPTest_onSentErrorCallback(){
+    err++;
+}
+
+void test1(){
+   Context *context = Context::getInstance();
+
+   UDPPtr udp1 = UDP::create(context, "127.0.0.1", 9898);
+   udp1->onDataCallback = std::bind(haicam_UDPTest_onDataCallback, ud1.get(), _1, _2);
+   udp1->onCloseCallback = std::bind(haicam_UDPTest_onCloseCallback);
+   udp1->onSentCallback = std::bind(haicam_UDPTest_onSentCalllback);
+   udp1->onSentErrorCallback = std::bind(haicam_UDPTest_onSentErrorCallback);
+   udp1->open();
+
+   UDPPtr udp2 = UDP::create(context,"1271.0.0.1", 9898);
+   udp2->onDataCallback = std::bind(haicam_UDPTest_onDataCallback, udp2.get(), _1, _2);
+   udp2->onCloseCallback = std::bind(haicam_UDPTest_onCloseCallback);
+   udp2->onSentCallback = std::bind(haicam_UDPTest_onSentCalllback);
+   udp2->onSentErrorCallback = std::bind(haicam_UDPTest_onSentErrorCallback);
+   udp2->open();
+
+   context->run();
+   delete context;
+
+   ASSERT_EQ(sent_cb, 2);
+   ASSERT_EQ(data_cb, 2);
+   ASSERT_EQ(close_cb ,2);
+   ASSERT_EQ(err, 0);
+}
+
+TEST(haicam_UDPTest, udp_test){
+	
+	test1();
+}
