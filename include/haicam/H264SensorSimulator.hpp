250e001f2b57d316b175656d01c8bb042bd73dd0
Jaganmohan720 <67822144+Jaganmohan720@users.noreply.github.com>
2022-06-28 08:44:14 +0530

    Add files via upload

--- include/haicam/H264SensorSimulator.hpp
+++ include/haicam/H264SensorSimulator.hpp
@@ -1,37 +1,60 @@
 #pragma once
 #include "haicam/ImageSensor.hpp"
 #include "haicam/Runnable.hpp"
+#include <iostream>
+#include <fstream>
 #include <list>
 #include <memory>
 
 namespace haicam
 {
 
-    class H264SensorSimulator : public ImageSensor
+    class H264SensorSimulator : public ImageSensor  //class H264 derived from class Imagesensor
     {
     private:
-        H264SensorSimulator();
+        H264SensorSimulator();  //constructor
 
     public:
-        ~H264SensorSimulator();
+        ~H264SensorSimulator(); //destructor
 
         void run();
     };
 
-    H264SensorSimulator::H264SensorSimulator() : ImageSensor()
+    H264SensorSimulator::H264SensorSimulator() : ImageSensor() // Aceesing class ouside fun using scope resolution 
     {
     }
 
-    std::shared_ptr<H264SensorSimulator> create()
+    std::shared_ptr<H264SensorSimulator> create()  //polymorphic function wrap
     {
         return std::shared_ptr<H264SensorSimulator>(new H264SensorSimulator);
     }
 
-    H264SensorSimulator::~H264SensorSimulator()
+    H264SensorSimulator::~H264SensorSimulator() //destuctor function
     {
     }
 
-    void H264SensorSimulator::run()
+    void H264SensorSimulator::run() //Reading Raw data from simulator.data 
     {
-    }
+
+       FILE *fp = fopen("h264.data","rb");
+
+         if(fp)
+             {
+              fseek (fp, 0, SEEK_END);
+              int size = ftell(fp);
+              fseek (fp, 1, SEEK_SET);
+
+              char * buffer = new char [size];
+              std::cout << "Reading " << size << " characters... ";
+              fread (buffer,size,1,fp);
+
+              delete[] buffer;
+              fclose(fp);
+             }
+
+             else
+                 {
+                  std::cout << "error:The file can not be found";
+                  fclose(fp);
+                 }
 }
